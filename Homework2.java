0d60bd88fef54b6ef230b405dc7a672f00953ddc
Ruben Martinez <48562282+rubenmartinez2914@users.noreply.github.com>
2020-01-24 13:50:53 -0600

    Update Homework2.java

--- Homework2.java
+++ Homework2.java
@@ -2,6 +2,12 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ 
+ 
+ Simple calculator
+ 
+ 
+ 
  */
 package homework2;
 import java.util.Scanner;
